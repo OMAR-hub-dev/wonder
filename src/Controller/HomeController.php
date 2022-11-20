7b27badda83cc275618f25b58513ddec080ad818
o.omar <oulaya.omar@gmail.com>
2022-11-20 21:45:11 +0100

    question page & détails question

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,60 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+
+
+        $questions =[
+            [
+                'id'=>1,
+                'title'=>'Essential WONDER',
+                'content'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias harum atque ducimus ullam temporibus, sequi rerum natus consequatur modi libero!',
+                'rating'=>20,
+                'auteur'=>[
+                    'nom'=>'Jon J. Martinez',
+                    'avatar'=>'https://randomuser.me/api/portraits/men/79.jpg'
+                ],
+                'nombreOfResponse'=>15
+            ],
+            [
+                'id'=>2,
+                'title'=>'Works Only Under These Conditions',
+                'content'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias harum atque ducimus ullam temporibus, sequi rerum natus consequatur modi libero!',
+                'rating'=>-5,
+                'auteur'=>[
+                    'nom'=>'Perrin Dupont',
+                    'avatar'=>'https://randomuser.me/api/portraits/men/24.jpg'
+                ],
+                'nombreOfResponse'=>20
+            ],
+            [
+                'id'=>3,
+                'title'=>'WONDER You Can\'t Afford ',
+                'content'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias harum atque ducimus ullam temporibus, sequi rerum natus consequatur modi libero!',
+                'rating'=>0,
+                'auteur'=>[
+                    'nom'=>'Wyatt Quiron',
+                    'avatar'=>'https://randomuser.me/api/portraits/men/11.jpg'
+                ],
+                'nombreOfResponse'=>25
+            ],
+            [
+                'id'=>4,
+                'title'=>'How To Start WONDER With Less Than $100',
+                'content'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias harum atque ducimus ullam temporibus, sequi rerum natus consequatur modi libero!',
+                'rating'=>35,
+                'auteur'=>[
+                    'nom'=>'Ferrau Fontaine',
+                    'avatar'=>'https://randomuser.me/api/portraits/men/28.jpg'
+                ],
+                'nombreOfResponse'=>30
+            ],
+          
+
+        ];
+
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'questions' => $questions,
         ]);
     }
 }
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\QuestionType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'question_form')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $formquestion = $this->createForm(QuestionType::class);
+
+        $formquestion->handleRequest($request);
+         if($formquestion->isSubmitted() && $formquestion->isValid()){
+            
+         }
+
         return $this->render('question/index.html.twig', [
-            'controller_name' => 'QuestionController',
+            'form' => $formquestion->createView(),
+        ]);
+    }
+
+
+    #[Route('/question/{id}', name: 'question_show')]
+    public function show(Request $request, string $id): Response
+    {
+       $question =  [
+        'id'=>3,
+        'title'=>'WONDER You Can\'t Afford ',
+        'content'=>'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias harum atque ducimus ullam temporibus, sequi rerum natus consequatur modi libero!',
+        'rating'=>0,
+        'auteur'=>[
+            'nom'=>'Wyatt Quiron',
+            'avatar'=>'https://randomuser.me/api/portraits/men/11.jpg'
+        ],
+        'nombreOfResponse'=>25
+    ];
+
+        return $this->render('question/show.html.twig', [
+            'question'=>$question,
         ]);
     }
 }
